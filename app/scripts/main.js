1eb372b48f77be0627de11957cbe1eccc68109cb
wab <wab2com@gmail.com>
2014-05-12 22:24:20 +0200

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+console.log('\'Allo \'Allo!');
+
+$(function() {
+	'use strict';
+    var $menu = $('nav#menuActivites');
+    $menu.mmenu({
+    }, {
+        selectedClass: 'active'
+		
+    });
+});
